0dc94249b2eed9104fdb1e3279196685360778e9
nanna-dk <nanna@e-nanna.dk>
2021-11-05 14:55:07 +0100

    Updated packages

--- KU-extensions/ku_global_layout/vendor/bin/fluid.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../typo3fluid/fluid/bin/fluid
-php "%BIN_TARGET%" %*
--- KU-extensions/ku_global_layout/vendor/bin/typo3.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../typo3/cms-cli/typo3
-php "%BIN_TARGET%" %*
