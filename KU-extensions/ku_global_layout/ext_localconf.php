39d5732fb472773bfee7f2fc6beb91c194c81226
nanna-dk <nanna@e-nanna.dk>
2021-11-12 09:14:20 +0100

    Cleanup

--- KU-extensions/ku_global_layout/ext_localconf.php
+++ KU-extensions/ku_global_layout/ext_localconf.php
@@ -10,3 +10,17 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_global_layout'] = 'EXT:ku_glob
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ku_global_layout/Configuration/TsConfig/Page/All.tsconfig">');
+
+
+/***************
+ * Register Icons
+ */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+
+$iconRegistry->registerIcon(
+   // use same identifier as used in TSconfig for icon
+   'modals-button',
+   \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+   // font-awesome identifier ('external-link-square')
+   ['name' => 'external-link-square']
+);
