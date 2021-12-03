0ccbfb6a282ab0cb8c38fe10a16a081f527f7f44
nanna-dk <nanna@e-nanna.dk>
2021-12-03 10:24:06 +0100

    New font added

--- KU-extensions/ku_prototype/Resources/Public/JavaScript/Src/ku-custom.js
+++ KU-extensions/ku_prototype/Resources/Public/JavaScript/Src/ku-custom.js
@@ -5,7 +5,7 @@
  * ========================================================================*/
 
 
-var debounce = (func, wait, immediate) => {
+const debounce = (func, wait, immediate) => {
   // Use to delay function init, e.g. on window resizing or input autocomplete, etc..
   // Returns a function, that, as long as it continues to be invoked, will not
   // be triggered. The function will be called after it stops being called for
@@ -13,13 +13,14 @@ var debounce = (func, wait, immediate) => {
   // leading edge, instead of the trailing. Usage, call with time, e.g. $(window).on('resize', debounce(myFunction, 250));
   var timeout;
   return function () {
-    var context = this, args = arguments;
-    var later = () => {
+    let context = this,
+      args = arguments;
+    let later = () => {
       timeout = null;
       if (!immediate)
         func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
+    let callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow)
@@ -27,10 +28,10 @@ var debounce = (func, wait, immediate) => {
   };
 }
 
-var throttle = (func, wait) => {
+const throttle = (func, wait) => {
   // A note about debounce and throttle:
   // A debounce is utilized when you only care about the final state. For example, waiting until a user stops typing to // fetch typeahead search results. A throttle is best used when you want to handle all intermediate states but at a // controlled rate. For example, track the screen width as a user resizes the window and rearrange page content while // it changes instead of waiting until the user has finished.
-  var waiting = false;
+  let waiting = false;
   return function () {
     if (!waiting) {
       func.apply(this, arguments);
@@ -42,9 +43,9 @@ var throttle = (func, wait) => {
   };
 }
 
-function hasScrollbar() {
+const hasScrollbar = () => {
   // Check if page has scrollbar and if so add css variable. Used for full width styling.
-  var body = document.body;
+  let body = document.body;
   if (body) {
     if (window.innerWidth > body.clientWidth) {
       body.classList.add('has-scrollbar');
@@ -54,7 +55,8 @@ function hasScrollbar() {
     }
   }
 }
-window.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded', () => {
 
   hasScrollbar();
 
