96280b6dbcd5b098e46a7c862e0914a55eba720e
nanna-dk <nanna@e-nanna.dk>
2022-03-18 13:59:05 +0100

    Updated extension

--- KU-extensions/ku_prototype/ext_tables.php
+++ KU-extensions/ku_prototype/ext_tables.php
@@ -1,2 +1,5 @@
 <?php
 defined('TYPO3_MODE') || die();
+
+// Backend stylesheet
+$GLOBALS['TBE_STYLES']['skins']['ku_prototype']['stylesheetDirectories'][] = 'EXT:ku_prototype/Resources/Public/Css/Backend/';
