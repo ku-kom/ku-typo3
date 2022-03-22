ceecd127d333396e7877ddc6ade044860978e60f
nanna-dk <nanna@e-nanna.dk>
2022-03-22 12:51:54 +0100

    KU logo backend

--- KU-extensions/ku_prototype/ext_localconf.php
+++ KU-extensions/ku_prototype/ext_localconf.php
@@ -6,7 +6,7 @@ defined('TYPO3_MODE') || die();
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_prototype'] = 'EXT:ku_prototype/Configuration/RTE/Default.yaml';
 
 // KU sinmple editor
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['KU_simple'] = 'EXT:ku_prototype/Configuration/RTE/KU_simple.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_simple'] = 'EXT:ku_prototype/Configuration/RTE/KU_simple.yaml';
 
 // KU news editor
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_news'] = 'EXT:ku_prototype/Configuration/RTE/KU_news.yaml';
@@ -15,3 +15,12 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_news'] = 'EXT:ku_prototype/Con
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ku_prototype/Configuration/TsConfig/Page/All.tsconfig">');
+
+// Backend logo and login logo
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
+    'backendFavicon' => 'EXT:ku_prototype/Resources/Public/Icons/favicon.ico',
+    'backendLogo' => 'EXT:ku_prototype/Resources/Public/Images/Backend/ku.svg',
+    'loginLogo' => 'EXT:ku_prototype/Resources/Public/Images/Backend/ku-login.png',
+    'loginBackgroundImage' => 'EXT:ku_prototype/Resources/Public/Images/Backend/ku-login-background.jpg',
+    'loginFootnote' => 'Københavns Universitet',
+];
