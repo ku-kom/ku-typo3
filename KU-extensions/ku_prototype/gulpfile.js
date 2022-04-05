cf8f64326b3edda53561d0cc636e260930c3d973
nanna-dk <nanna@e-nanna.dk>
2022-04-05 10:34:50 +0200

    Setting up gulp

--- KU-extensions/ku_prototype/gulpfile.js
+++ KU-extensions/ku_prototype/gulpfile.js
@@ -1,39 +1,51 @@
-/* eslint-disable no-undef */
 'use strict';
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 const del = require('del');
 
 const sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'compressed'
-  };
+  errLogToConsole: true,
+  outputStyle: 'compressed'
+};
 
-  gulp.task('clean', () => {
-    return del([
-        './Resources/Public/Css/Dist',
-    ]);
+gulp.task('clean', () => {
+  return del([
+    './Resources/Public/Css/Dist'
+  ]);
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   return gulp.src('./Resources/Public/Scss/Theme/theme.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer({
-        cascade: false
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+      suffix: '.min'
     }))
     .pipe(gulp.dest('./Resources/Public/Css/Dist'));
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('./Resources/Public/JavaScript/Src/**/*.js')
-        // Todo: concatenate and uglify
-      .pipe(gulp.dest('./Resources/Public/JavaScript//Dist/*.min.js'));
-  });
+gulp.task('scripts', () => {
+  return gulp.src('./Resources/Public/JavaScript/Src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify({
+      mangle: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(concat('ku-scripts.min.js'))
+    .pipe(gulp.dest('./Resources/Public/JavaScript/Dist'));
+});
 
-gulp.task('watch', function () {
-  gulp.watch('./Resources/Public/Scss/**/*.scss', gulp.series(['clean', 'sass']));
-  // gulp.watch('app/js/*.js', gulp.series('scripts'));
-  // gulp.watch('app/img/*', gulp.series('images'));
+gulp.task('watch', () => {
+  gulp.watch('./Resources/Public/Scss/Theme/**/*.scss', gulp.series(['clean', 'sass']));
+  gulp.watch('./Resources/Public/JavaScript/Src/**/*.js', gulp.series(['scripts']));
 });
