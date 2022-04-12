1e0daabae028c4bb8373de16b4c466c0a908f1e9
nanna-dk <nanna@e-nanna.dk>
2022-04-12 10:25:21 +0200

    Menua aria states

--- KU-extensions/ku_prototype/Resources/Public/JavaScript/Src/ku-mainnavigation.js
+++ KU-extensions/ku_prototype/Resources/Public/JavaScript/Src/ku-mainnavigation.js
@@ -6,22 +6,65 @@
 (function () {
   'use strict'
 
-  const menu_arrow = document.querySelectorAll('.menu_arrow');
+  window.addEventListener('DOMContentLoaded', function () {
 
-  /**
-   * Set accessible attributes to menu items
-   */
-  const setAriaAttr = (el) => {
-    el.setAttribute(
-      'aria-expanded',
-      el.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-    );
-  };
+    /**
+     * Solution to set appropiate aria-axpanded states in menus.
+     * 
+     */
+    function navbarPointerOver(element) {
+      let toggle = document.querySelector('.navbar-toggler');
+      if (window.getComputedStyle(toggle).display === 'none' && element.classList.contains('open') === false) {
+        element.querySelector('.menu_arrow').setAttribute('aria-expanded', 'true');
+      }
+    }
 
-  for (const btn of menu_arrow) {
-    btn.addEventListener('click', () => {
-      setAriaAttr(btn);
+    function navbarPointerLeave(element) {
+      let toggle = document.querySelector('.navbar-toggler');
+      if (window.getComputedStyle(toggle).display === 'none') {
+        element.querySelector('.menu_arrow').setAttribute('aria-expanded', 'false');
+      }
+    }
+
+    Array.from(document.querySelectorAll('li.has-children')).forEach(function (element) {
+      element.addEventListener('pointerover', (e) => {
+        if (e.pointerType === "mouse") {
+          navbarPointerOver(element);
+        }
+      });
+      element.addEventListener('mouseenter', () => {
+        if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+          navbarPointerOver(element);
+        }
+      });
+      element.addEventListener('pointerleave', (e) => {
+        if (e.pointerType === "mouse") {
+          navbarPointerLeave(element);
+        }
+      });
+      element.addEventListener('mouseleave', () => {
+        if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+          navbarPointerLeave(element);
+        }
+      });
     });
-  }
+
+    Array.from(document.querySelectorAll('.menubar li')).forEach(function (element) {
+      element.addEventListener('click', (e) => {
+        let listElement = element.parentElement;
+        if (listElement.classList.contains('has-children')) {
+          let listElementSiblings = listElement.parentElement.querySelectorAll('.menu_arrow');
+          Array.from(listElementSiblings).forEach(function (listElementsSibling) {
+            listElementsSibling.setAttribute('aria-expanded', 'false')
+          });
+          listElement.setAttribute('aria-expanded', 'true')
+          listElement.querySelector('.menu_arrow').setAttribute('aria-expanded', 'true');
+          e.stopImmediatePropagation();
+          e.preventDefault();
+          return false;
+        }
+      });
+    });
+  });
 
 })()
